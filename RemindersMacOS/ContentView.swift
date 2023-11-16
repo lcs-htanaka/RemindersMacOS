3392b65f5185622777b382653879eb1ce06c9f95
Hiro Tanaka <HTanaka@lcs.on.ca>
2023-11-16 10:06:42 -0500

    Initial Commit

---
+++ RemindersMacOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RemindersMacOS
+//
+//  Created by 田中宏貴 on 2023-11-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RemindersMacOS/RemindersMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersMacOSApp.swift
+//  RemindersMacOS
+//
+//  Created by 田中宏貴 on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct RemindersMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
